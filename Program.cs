68dbf14f89a7e258c44916c3dfa2ae594a28b631
Mauricio <mauvillaarias10@gmail.com>
2026-09-23 12:36:38 -0500

    Impresion de notas

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+static void imprimirNotas(int[] notas)
+{
+    for (int i = 0; 0 < notas.Length; i++)
+    {
+    System.Console.WriteLine($"nota {i + 1}: {notas[i]}");
+    }
+}
+
+int [] notas = {15,18,12,17,20};
+imprimirNotas(notas);
