a4c6e39817f76e9745cc8a796178f02be04531de
Mauricio <mauvillaarias10@gmail.com>
2026-09-23 13:18:11 -0500

    Promedio de notas

--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
 ﻿
-using System.Diagnostics.CodeAnalysis;
+using System;
 
-static void imprimirNotas(int[] notas)
+static void imprimirNotas(int[] notasx)
 {
-    for (int i = 0; 0 < notas.Length; i++)
+    for (int i = 0; i < notasx.Length; i++)
     {
-    System.Console.WriteLine($"nota {i + 1}: {notas[i]}");
+    System.Console.WriteLine($"nota {i + 1}: {notasx[i]}");
     }
+}
+
+static double PromedioNotas(int[] notasx)
+{
+    int suma = 0;
+    for (int i = 0; i < notasx.Length; i++)
+    {
+        suma += notasx[i];
+    }
+    return (double)suma / notasx.Length;
 }
 
 int [] notas = {15,18,12,17,20};
 imprimirNotas(notas);
+
+double promedio =  PromedioNotas(notas);
+System.Console.WriteLine($"Promedio de las notas: {promedio:F2}");
